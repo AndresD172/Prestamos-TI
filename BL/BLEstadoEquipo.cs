37775dae415c2c99d5677f006c0cc2cbb06ef184
Alejandro Rodriguez Montero <alerodriguez.montero@gmail.com>
2024-08-13 07:49:29 -0600

    Finalizar BL

---
+++ BL/BLEstadoEquipo.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BLEstadoEquipo
+    {
+        public Respuesta RegistrarEstadoEquipo(EntidadEstadoEquipo estadoequipo)
+        {
+            DALEstadoEquipo DalEstadoEquipo = new DALEstadoEquipo();
+
+            return DalEstadoEquipo.RegistrarEstadoEquipo(estadoequipo);
+        }
+        public Respuesta ListarEstadoEquipo(string cTexto)
+        {
+            DALEstadoEquipo DalEstadoEquipo = new DALEstadoEquipo();
+
+            return DalEstadoEquipo.ListarEstadoEquipo(cTexto);
+        }
+        public Respuesta EliminarEstadoEquipo(int IdEstadoEquipo)
+        {
+            DALEstadoEquipo DalEstadoEquipo = new DALEstadoEquipo();
+
+            return DalEstadoEquipo.EliminarEstadoEquipo(IdEstadoEquipo);
+        }
+        public Respuesta ActualizarEstadoEquipo(EntidadEstadoEquipo estadoequipo)
+        {
+            DALEstadoEquipo DalEstadoEquipo = new DALEstadoEquipo();
+
+            return DalEstadoEquipo.ActualizarEstadoEquipo(estadoequipo);
+        }
+    }
+}
