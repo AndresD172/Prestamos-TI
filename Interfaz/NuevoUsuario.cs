dc317b30870b35c47a2eaef226a461bd4d7396c0
Sharon Salgado <sharon.salgado@uned.cr>
2024-08-15 18:32:58 -0600

    Funcionalidad de "Nuevo Usuario"

--- Interfaz/NuevoUsuario.cs
+++ Interfaz/NuevoUsuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidades;
+using BL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +19,36 @@ namespace InterfazGráfica
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void btnGuardarUsuario_Click(object sender, EventArgs e)
         {
+            //Comprueba que todos los campos de datos hayan sido ingresados
+            if (txtNombreUsuario.Text == String.Empty || txtApellidosUsuario.Text == String.Empty || txtCorreoUsuario.Text == String.Empty ||
+            txtCarnetUsuario.Text == String.Empty || txtContraseñaUsuario.Text == String.Empty) {
+                MessageBox.Show("Ingrese todos los datos requeridos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                EntidadTécnico tecnico = new EntidadTécnico();
+                Respuesta respuesta;
+                //Asigna los datos que se ingresaron
+                tecnico.Nombre = txtNombreUsuario.Text.Trim();
+                tecnico.Apellidos = txtApellidosUsuario.Text.Trim();
+                tecnico.CorreoElectrónico = txtCorreoUsuario.Text.Trim();
+                tecnico.NúmeroCarnet = txtCarnetUsuario.Text.Trim();
+                tecnico.Contraseña = txtContraseñaUsuario.Text.Trim();
 
+                respuesta=BLTécnico.RegistrarTécnico(tecnico);
+
+                //Detección de errores
+                if (respuesta.CódigoEstado==0)
+                {
+                    MessageBox.Show(respuesta.Contenido, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(respuesta.Contenido, "Aviso del sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
